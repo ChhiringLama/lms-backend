bc89a261881b9641fc640d82e7a91ce3684fda9d
Chhiring Lama <111995626+ChhiringLama@users.noreply.github.com>
2026-04-08 00:12:20 +0545

    Refactor CORS configuration to use a callback

--- index.js
+++ index.js
@@ -22,9 +22,17 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://lms-frontend-render.onrender.com/"],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
-  }),
+  })
 );
 
 //APIS
